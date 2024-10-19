b85310769e9e362315d206dad59ece8062c52e98
unknown <amanyadav16544@gmail.com>
2024-10-19 10:20:56 +0530

    finished setting up the enviroment

---
+++ app/(site)/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Home = () => {
+  return <div className="text-green-500">Hello from RhythmNet (Site)!</div>;
+};
+
+export default Home;
--- app/page.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const Home = () => {
-  return (
-    <div className='text-green-500'>Hello from RhythmNet</div>
-  )
-}
-
-export default Home
